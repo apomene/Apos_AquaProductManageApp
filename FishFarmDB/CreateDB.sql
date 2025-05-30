ee56fdf7107a854823b8b14d03329c5c96b40a20
Apo Mene <apomene@yahoo.gr>
2025-05-30 16:56:01 +0300

    update DB schema

--- FishFarmDB/CreateDB.sql
+++ FishFarmDB/CreateDB.sql
@@ -40,33 +40,4 @@ CREATE TABLE FishTransfers (
 );
 GO
 
--- Table: FishTransferDetails
-CREATE TABLE FishTransferDetails (
-    TransferDetailId INT IDENTITY(1,1) PRIMARY KEY,
-    TransferId INT NOT NULL,
-    DestinationCageId INT NOT NULL,
-    Quantity INT NOT NULL CHECK (Quantity > 0),
-    CONSTRAINT FK_TransferDetails_Transfer FOREIGN KEY (TransferId) REFERENCES FishTransfers(TransferId),
-    CONSTRAINT FK_TransferDetails_DestinationCage FOREIGN KEY (DestinationCageId) REFERENCES Cages(CageId)
-);
-GO
-
--- Table: DailyStockBalances
-CREATE TABLE DailyStockBalances (
-    CageId INT NOT NULL,
-    BalanceDate DATE NOT NULL,
-    Quantity INT NOT NULL CHECK (Quantity >= 0),
-    CONSTRAINT PK_DailyStock PRIMARY KEY (CageId, BalanceDate),
-    CONSTRAINT FK_StockBalance_Cage FOREIGN KEY (CageId) REFERENCES Cages(CageId)
-);
-GO
-
--- View: Pivot-style Mortality Summary
--- (For use in front-end/reporting, not materialized)
--- Can be queried like:
--- SELECT CageId, YEAR(MortalityDate) AS Year, MONTH(MortalityDate) AS Month, SUM(Quantity) AS TotalMortalities
--- FROM Mortalities
--- GROUP BY CageId, YEAR(MortalityDate), MONTH(MortalityDate);
-
--- You can create an indexed view if necessary, depending on performance needs
 
