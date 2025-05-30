4231b0f0c7c90cb76de768558e8f08bb67713da6
Apo Mene <apomene@yahoo.gr>
2025-05-30 10:11:49 +0300

    Add 2 new Views/Forms: BalanceForm and MortalityPivotForm

---
+++ Views/BalanceForm.cs
@@ -0,0 +1,46 @@
+﻿using Apos_AquaProductManageApp.Model;
+using Apos_AquaProductManageApp.Presenters;
+
+using static Apos_AquaProductManageApp.Interfaces.ViewInterfaces;
+
+namespace Apos_AquaProductManageApp.Views
+{
+    public partial class BalanceForm : Form, IBalanceView
+    {
+        private BalancePresenter _presenter = null!;
+        private DataGridView gridBalances = null!;
+        private DateTimePicker datePicker = null!;
+
+        public BalanceForm()
+        {
+            InitializeComponent();
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            this.Text = "Stock Balance";
+            this.Dock = DockStyle.Fill;
+
+            datePicker = new DateTimePicker { Dock = DockStyle.Top };
+            gridBalances = new DataGridView { Dock = DockStyle.Fill, ReadOnly = true, AutoGenerateColumns = true };
+
+            datePicker.ValueChanged += (s, e) => _presenter.LoadBalances(datePicker.Value.Date);
+
+            Controls.Add(gridBalances);
+            Controls.Add(datePicker);
+        }
+
+        public void SetPresenter(BalancePresenter presenter)
+        {
+            _presenter = presenter;
+            _presenter.LoadBalances(datePicker.Value.Date);
+        }
+
+        public void DisplayBalances(List<StockBalance> balances)
+        {
+            gridBalances.DataSource = balances;
+        }
+    }
+
+}
---
+++ Views/MortalityPivotForm.cs
@@ -0,0 +1,39 @@
+﻿using Apos_AquaProductManageApp.Model;
+using Apos_AquaProductManageApp.Presenters;
+using static Apos_AquaProductManageApp.Interfaces.ViewInterfaces;
+
+namespace Apos_AquaProductManageApp.Views
+{
+    public partial class MortalityPivotForm : Form, IMortalityPivotView
+    {
+        private MortalityPivotPresenter _presenter = null!;
+        private DataGridView gridPivot = null!;
+
+        public MortalityPivotForm()
+        {
+            InitializeComponent();
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            this.Text = "Mortality Pivot Analysis";
+            this.Dock = DockStyle.Fill;
+
+            gridPivot = new DataGridView { Dock = DockStyle.Fill, ReadOnly = true, AutoGenerateColumns = true };
+            Controls.Add(gridPivot);
+        }
+
+        public void SetPresenter(MortalityPivotPresenter presenter)
+        {
+            _presenter = presenter;
+            _presenter.LoadPivot();
+        }
+
+        public void DisplayPivot(List<MortalityPivot> pivot)
+        {
+            gridPivot.DataSource = pivot;
+        }
+    }
+
+}
